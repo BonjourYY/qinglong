b3dcc3a47671b886d2010fba0dcc66df9dc845c1
范勤阳 <fqymtu@gmail.com>
2025-09-23 10:48:28 +0800

    4

---
+++ cg/checkin.ts
@@ -0,0 +1,20 @@
+let username = "";
+let password = "";
+
+// 获取环境列表
+const { data: cgUsername } = QLAPI.getEnvs({ searchValue: "cgUsername" });
+username = cgUsername.value;
+
+const { data: cgPassword } = QLAPI.getEnvs({ searchValue: "cgPassword" });
+password = cgPassword.value;
+
+// 常观签到
+import { login } from "./api";
+
+const loginRes = await login({
+  username,
+  password,
+  with_applications: 0,
+});
+
+console.log("loginRes", loginRes);
--- index.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-console.log(axios);
-
-// 获取环境列表
-QLAPI.getEnvs({ searchValue: "dddd" }).then((x) => {
-  console.log("getEnvs", x);
-});
