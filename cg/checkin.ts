9dac0aa0a0ad0ccdb42ce9457eb601af302ee0b0
范勤阳 <fqymtu@gmail.com>
2025-09-25 00:32:42 +0800

    444

--- cg/checkin.ts
+++ cg/checkin.ts
@@ -3,13 +3,13 @@ let password = "";
 
 // 获取环境列表
 const { data: cgUsername } = QLAPI.getEnvs({ searchValue: "cgUsername" });
-username = cgUsername.value;
+// username = cgUsername.value;
 
 const { data: cgPassword } = QLAPI.getEnvs({ searchValue: "cgPassword" });
-password = cgPassword.value;
+// password = cgPassword.value;
 
 // 常观签到
-import { login } from "./api";
+import { login } from "./api/index.ts";
 
 const loginRes = await login({
   username,
