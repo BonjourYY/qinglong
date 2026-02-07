b74cd5060387714cf4fe8e8d13b3ba1a6f49f9bc
范勤阳 <fqymtu@gmail.com>
2026-02-07 13:33:13 +0800

    1

--- script/cg_checkIn.ts
+++ script/cg_checkIn.ts
@@ -1,52 +1,54 @@
-import HTTP from "../utils/http.ts";
+// import HTTP from "../utils/http.ts";
 
-let username = "";
-let password = "";
-let token = "";
+// let username = "";
+// let password = "";
+// let token = "";
 
-// 登录
-const login = async () => {
-  // 获取环境变量
-  QLAPI.getEnvs({ searchValue: "cgUsername" }).then((x) => {
-    username = x.data[0].value;
-    console.log(username);
-  });
-  QLAPI.getEnvs({ searchValue: "cgPassword" }).then((x) => {
-    password = x.data[0].value;
-    console.log(password);
-  });
+// // 登录
+// const login = async () => {
+//   // 获取环境变量
+//   QLAPI.getEnvs({ searchValue: "cgUsername" }).then((x) => {
+//     username = x.data[0].value;
+//     console.log(username);
+//   });
+//   QLAPI.getEnvs({ searchValue: "cgPassword" }).then((x) => {
+//     password = x.data[0].value;
+//     console.log(password);
+//   });
 
-  // 调用接口
-  const { data } = await HTTP.post("https://uc.cztv.tv/api/auth/login/pwd", {
-    username,
-    password,
-    with_applications: 1,
-  });
+//   // 调用接口
+//   const { data } = await HTTP.post("https://uc.cztv.tv/api/auth/login/pwd", {
+//     username,
+//     password,
+//     with_applications: 1,
+//   });
 
-  token = data.access_token_for_clients["2024110112345678"];
-};
+//   token = data.access_token_for_clients["2024110112345678"];
+// };
 
-// 签到
-const checkin = async () => {
-  // 调用接口
-  await HTTP.post(
-    "https://kcz.cztv.tv/api/v1/user/checkin",
-    {
-      client_id: "rongmei",
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    },
-  );
-};
+// // 签到
+// const checkin = async () => {
+//   // 调用接口
+//   await HTTP.post(
+//     "https://kcz.cztv.tv/api/v1/user/checkin",
+//     {
+//       client_id: "rongmei",
+//     },
+//     {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     },
+//   );
+// };
 
-// 执行
-const fn = async () => {
-  await login();
-  await checkin();
-  console.log("✅✅✅ 签到成功 ✅✅✅");
-};
+// // 执行
+// const fn = async () => {
+//   await login();
+//   await checkin();
+//   console.log("✅✅✅ 签到成功 ✅✅✅");
+// };
 
-fn();
+// fn();
+
+console.log(123);
