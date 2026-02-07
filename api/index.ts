a26e92e3f88b4ab60231f3862382f4c9f5b30687
范勤阳 <fqymtu@gmail.com>
2026-02-07 16:54:59 +0800

    1

---
+++ api/index.ts
@@ -0,0 +1,32 @@
+import HTTP from "../utils/http.ts";
+// import { Res } from "./type.ts";
+
+// https://kcz.cztv.tv/api/v1
+
+// 获取 天下 栏目列表的文章
+export const getArticleList = ({ page }: { page: number }) => {
+  return HTTP.get(`homepage/column/80?page=${page}`);
+};
+
+// 获取 视频 栏目列表的视频
+export const getVideoList = ({ page }: { page: number }) => {
+  return HTTP.get(`homepage/column/75?page=${page}`);
+};
+
+// 获取文章详情数据
+export const getArticleDetail = ({ articleId }: { articleId: number }) => {
+  return HTTP.get(`article/${articleId}`);
+};
+
+// 点赞文章
+export const likeArticle = ({ articleId }: { articleId: number }) => {
+  return HTTP.post(`article/${articleId}/like`);
+};
+
+// 评论文章
+export const commentArticle = (
+  params: { articleId: number },
+  data: { content: string },
+) => {
+  return HTTP.post(`article/${params.articleId}/comment`, { data });
+};
--- script/cg_checkIn.ts
+++ script/cg_checkIn.ts
@@ -1,59 +1,15 @@
 /**
  * cron: 30 8 * * *
- * new Env('某某签到任务');
+ * new Env('常观每日任务');
  */
 
-// import HTTP from "../utils/http.ts";
+import { getArticleList } from "../api/index.ts";
 
-// let username = "";
-// let password = "";
-// let token = "";
+// 完成每日任务
 
-// // 登录
-// const login = async () => {
-//   // 获取环境变量
-//   QLAPI.getEnvs({ searchValue: "cgUsername" }).then((x) => {
-//     username = x.data[0].value;
-//     console.log(username);
-//   });
-//   QLAPI.getEnvs({ searchValue: "cgPassword" }).then((x) => {
-//     password = x.data[0].value;
-//     console.log(password);
-//   });
+const fn = async () => {
+  const { data } = await getArticleList({ page: 1 });
+  console.log(data);
+};
 
-//   // 调用接口
-//   const { data } = await HTTP.post("https://uc.cztv.tv/api/auth/login/pwd", {
-//     username,
-//     password,
-//     with_applications: 1,
-//   });
-
-//   token = data.access_token_for_clients["2024110112345678"];
-// };
-
-// // 签到
-// const checkin = async () => {
-//   // 调用接口
-//   await HTTP.post(
-//     "https://kcz.cztv.tv/api/v1/user/checkin",
-//     {
-//       client_id: "rongmei",
-//     },
-//     {
-//       headers: {
-//         Authorization: `Bearer ${token}`,
-//       },
-//     },
-//   );
-// };
-
-// // 执行
-// const fn = async () => {
-//   await login();
-//   await checkin();
-//   console.log("✅✅✅ 签到成功 ✅✅✅");
-// };
-
-// fn();
-
-console.log(123);
+fn();
