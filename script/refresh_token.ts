b3ad65cd8c509b013e81f0755be11ffab6435dbd
范勤阳 <fqymtu@gmail.com>
2026-05-28 23:28:28 +0800

    Update refresh_token.ts

--- script/refresh_token.ts
+++ script/refresh_token.ts
@@ -3,16 +3,24 @@
  * new Env('刷新常观 Token');
  */
 
+let username: string;
+let password: string;
+
+const getUsername = QLAPI.getEnvs({ searchValue: "cg_username" }).then((x) => {
+  username = x.data[0].value;
+});
+
+const getPassword = QLAPI.getEnvs({ searchValue: "cg_password" }).then((x) => {
+  password = x.data[0].value;
+});
+
 const refresToken = async () => {
+  await Promise.all([getUsername, getPassword]);
   const res = await fetch("https://uc.cztv.tv/api/auth/login/pwd", {
     method: "POST",
     body: JSON.stringify({
-      username: QLAPI.getEnvs({ searchValue: "cg_username" }).then((x) => {
-        return x.data[0].value;
-      }),
-      password: QLAPI.getEnvs({ searchValue: "cg_password" }).then((x) => {
-        return x.data[0].value;
-      }),
+      username: username,
+      password: password,
       with_applications: 1,
     }),
   });
